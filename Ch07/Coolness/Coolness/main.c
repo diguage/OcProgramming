54d0b31992e6af4a453ff29a6f78b2f1986fb7fd
D瓜哥 <leejun119@gmail.com>
2014-05-24 15:31:20 +0800

    完成第七章循环

---
+++ Ch07/Coolness/Coolness/main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  Coolness
+//
+//  Created by diguage on 14-5-24.
+//  Copyright (c) 2014年 D瓜哥，http://www.diguage.com/. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    for (int i = 0; i<10; i++) {
+        printf("Aaron is Cool.\n");
+    }
+    
+    int i = 0;
+    while (i < 10) {
+        printf("Aaron is Cool.\n");
+        i++;
+    }
+    return 0;
+}
+
